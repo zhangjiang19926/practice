9efddc6f2e1aad461c8a8c11d864c5edf863e80c
zhangjiang19926 <zhangxiaojian12345gmail.com>
2016-03-26 18:31:05 -0700

    2016.3.27 modified

--- practice/ctraining/student.c
+++ practice/ctraining/student.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void Sort(float *grade, int length)
+void Sort(float *grade, int length)/*利用冒泡进行排序的函数*/
 {
 	float tmp;
 	int i, j;
@@ -16,18 +16,36 @@ void Sort(float *grade, int length)
 			}
 	}
 }
-void Input(float *grade, int length)
+void Input(char stuName[][10], int *stuNum, float *grade, int length)/*输入成绩和学生姓名*/
 {
-	int i;
+	int i, j, flag = 1;
+	char c;
 	for (i = 0; i < length; ++i) 
+	{
+		printf("Please input %d student's information\n", i + 1);
+		printf("%d's studentname = ", i + 1);/*输入名字*/
+		scanf("%s", stuName[i]);/*以字符串的节奏输入到数组中*/
+		flag = 1;
+		printf("\n");
+		printf("%d's studentnumber = ", i + 1);/*输入学生学号*/
+		scanf("%d", stuNum + i);
+
+		printf("\n");
+		printf("%d student's grade = ", i + 1);/*输入学生成绩*/
 		scanf("%f", grade + i);
+		printf("\n");
+		c = getchar();
+	}
 }
 
-void Output(float *grade, int length)
+void Output(char stuName[][10], float *grade, int length)/*输出成绩及姓名*/
 {
-	int i;
+	int i, j, flag = 1;
 	for (i = 0; i < length; ++i)
-		printf("%d's grade is %f\n", i, *(grade + i));
+	{
+		printf("%s's grade", *(stuName + i));
+		printf("%d's grade is %f\n", i + 1, *(grade + i));
+	}
 }
 
 void Insert(float *grade, int length)
@@ -37,9 +55,11 @@ void Insert(float *grade, int length)
 
 int main(int argc, char **argv)
 {
-	float grade[11];	
-	Input(grade, 11);
-	Sort(grade, 11);
-	Output(grade, 11);
+	int stuNum[2];
+	float grade[2];	
+	char stuName[2][10];
+	Input(stuName, stuNum, grade, 2);
+	Sort(grade, 2);
+	Output(stuName, grade, 2);
 	return 0;
 }
