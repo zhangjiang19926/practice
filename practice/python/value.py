fe65fae5d38371759a3deb79c0ca0bb69e7d1e2d
zhangjiang19926 <zhangxiaojian12345gmail.com>
2016-03-29 03:05:09 -0700

    2016.3.29 modified

---
+++ practice/python/value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Filename:value.py
+n = 123
+f = 456.789
+s1 = 'Hello, World'
+s2 = 'Hello, \'Adan\''
+s3 = r'Hello, "Bart"'
+s4 = r'''hello,
+   Lisa!'''
+print(n, f, s1, s2, s3, s4)
+print('''hello\n
+wrold %s''')
