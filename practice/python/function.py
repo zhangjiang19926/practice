a6feef17c3494837802c928c2bfe4d4dc35bb176
zhangjiang19926 <zhangxiaojian12345@gmail.com>
2016-03-31 22:49:17 -0700

    2016.4.01 add

--- practice/python/function.py
+++ practice/python/function.py
@@ -11,4 +11,30 @@ def move(x, y, step, angle = 0):
 	nx = x + step * math.cos(angle)
 	ny = y + step * math.cos(angle)
 	return nx, ny
-print move(1, 2, 3,2)
+
+def enroll(name, gender, age = 6, city = "Beijing"):#default parameter
+	print "name:", name
+	print "gender:", gender
+	print "age:", age
+	print "city:", city
+
+def add_end(L = []):
+	L.append("END")
+	return L
+L =  add_end(["abcdefg"])
+
+def add_end(L = None):
+	if L is None:
+		L = []
+	L.append("END")
+	return L
+L =  add_end(["abcdefg"])
+print L
+print add_end()
+print add_end()
+print add_end()
+print add_end()
+print add_end()
+print add_end()
+print add_end()
+
