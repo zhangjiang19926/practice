56e9a70aef6db7ccdbf829f472c12cd32dc2c982
zhangjiang19926 <zhangxiaojian12345@gmail.com>
2016-03-31 18:53:12 -0700

    2016.04.01 commit

---
+++ practice/python/function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python 
+#Filename:function.py
+def my_abs(x):
+	if x >= 0:
+		return x
+	else:
+		return -x
+import math
+
+def move(x, y, step, angle = 0):
+	nx = x + step * math.cos(angle)
+	ny = y + step * math.cos(angle)
+	return nx, ny
+print move(1, 2, 3,2)
