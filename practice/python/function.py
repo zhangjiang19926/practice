344f9269f551ababfd041e1addde6dbd054828d2
zhangjiang19926 <zhangxiaojian12345@gmail.com>
2016-04-13 02:08:32 -0700

    2016.04.13 modified in lab and add partial

--- practice/python/function.py
+++ practice/python/function.py
@@ -270,4 +270,9 @@ def test1():
 	print "doing1"
 test1()
 #test1()
-
+def int2(x, base = 2):
+	return int(x, base)
+print int2("10")
+import functools
+int2 = functools.partial(int, base = 2)
+print int2("10")
